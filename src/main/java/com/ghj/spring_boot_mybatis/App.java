94843de0595c049a6919ca1ee8dfb6a5de26c4af
Your Name <you@example.com>
2017-03-07 14:30:10 +0800

    mybatis

---
+++ src/main/java/com/ghj/spring_boot_mybatis/App.java
@@ -0,0 +1,14 @@
+package com.ghj.spring_boot_mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.ghj.*")
+public class App 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
